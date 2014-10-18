1ae743d8a8315ca161af3ff5bd2caea7ff11fd62
Nicolas Perriault <nperriault@gmail.com>
2014-10-18 16:34:01 +0200

    Full screen support on mobile devices.

--- game.jsx
+++ game.jsx
@@ -12,6 +12,29 @@ var CELL_TAP = "tap";
 var CELL_DONE = "done";
 var CELL_DEAD = "dead";
 
+function toggleFullScreen() {
+  // http://www.html5rocks.com/en/mobile/fullscreen/
+  // yeah, I'm crying blood too.
+  var doc = window.document;
+  var docEl = doc.documentElement;
+  var requestFullScreen = docEl.requestFullscreen ||
+                          docEl.mozRequestFullScreen ||
+                          docEl.webkitRequestFullScreen ||
+                          docEl.msRequestFullscreen;
+  var cancelFullScreen = doc.exitFullscreen ||
+                         doc.mozCancelFullScreen ||
+                         doc.webkitExitFullscreen ||
+                         doc.msExitFullscreen;
+  if(!doc.fullscreenElement &&
+     !doc.mozFullScreenElement &&
+     !doc.webkitFullscreenElement &&
+     !doc.msFullscreenElement) {
+    requestFullScreen.call(docEl);
+  } else {
+    cancelFullScreen.call(doc);
+  }
+}
+
 function getAvailableWindowSize() {
   var availHeight = window.innerHeight - 200; // ~ title height… FIXME.
   return window.innerWidth > availHeight ?
@@ -405,4 +428,7 @@ var Game = React.createClass({
   }
 });
 
-React.renderComponent(<Game />, document.body);
+window.addEventListener("DOMLOaded", function() {
+  toggleFullScreen();
+  React.renderComponent(<Game />, document.body);
+});
